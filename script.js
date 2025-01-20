09d6b6d5654c3d9ed1279e3290cde5dd79666c22
Vanka Matouš <vankamatous235@student.voskh.cz>
2025-01-20 15:38:33 +0100

    hodina

--- script.js
+++ script.js
@@ -6,13 +6,26 @@ const pocetMat = document.getElementById("pocetMat");
 // kód vykreslení obrázků
 const pat = "<img src='img/pat.png'>";
 
+const mat = "<img src='img/mat.png'>";
+
 let kodFigurek = "";
 let i=0;
-while (i<10) {
+let pPat=0;
+let pMat=0;
+let patMat = false;
+while (i<6) {
   i++;
-  kodFigurek += "<div>" + pat + "</div>";
+
+  if (Math.random ()<0.5){
+    kodFigurek +="<div>" + pat + "</div>";
+  }
+  else {
+    kodFigurek +="<div>" + mat + "</div>";
+  }
+  patMat=!patMat;
 }
+
 figurky.innerHTML = kodFigurek; // vloží kód řady obrázků, kterou požadujeme vykreslit
-pocetPat.innerText = i;         // vypíše počet vykreslených figurek Pata (všech deset)
-pocetMat.innerText = 0;         // vypíše počet vykreslenách figurek Mata (žádný)
+pocetPat.innerText = pMat;         // vypíše počet vykreslených figurek Pata (všech deset)
+pocetMat.innerText = pPat;         // vypíše počet vykreslenách figurek Mata (žádný)
 
